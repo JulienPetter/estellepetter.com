af51e1a717665351b69215d571c9cb248b6d05c8
Julien Petter <25220546+JulienPetter@users.noreply.github.com>
2021-06-23 10:43:27 +0200

    Remove Google Maps integration

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Initialization of Google Maps
-function initMap () {
-    var coordinates = {lat: 46.117809, lng: 6.037543};
-    var map = new google.maps.Map(document.getElementById("map"), {center: coordinates, zoom: 17});
-    var marker = new google.maps.Marker({position: coordinates, map: map})
-}
